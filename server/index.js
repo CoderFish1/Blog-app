9629fd5b4b55f25b28a7e87356354296101c1f4f
CoderFish1 <shreypandey22005@gmail.com>
2026-01-09 06:52:32 +0530

    backend

--- server/index.js
+++ server/index.js
@@ -5,13 +5,18 @@ const mongoose = require("mongoose");
 const User = require('./models/User')
 const bcrypt = require("bcryptjs");
 const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser')
 
 const salt = bcrypt.genSaltSync(10);
 // Store hash in your password DB
 const secret = 'agasfjb3wljhbt3t2wgaihasiha';
 
-app.use(cors())
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true,
+}));
 app.use(express.json());   
+app.use(cookieParser());   
 
 require('dotenv').config()
 
@@ -38,13 +43,40 @@ app.post("/register", async (req, res) => {
 app.post('/login', async (req, res)=>{
   const {username, password} = req.body;
   const userDoc = await User.findOne({username})
+  if (!userDoc) return res.status(400).json("User not found");
+
   const passOK = bcrypt.compareSync(password, userDoc.password); // true
   if(passOK){
     //loggedIn
-    jwt.sign({username, id:userDoc._id}, secret, {})
+    jwt.sign({username, id:userDoc._id}, secret, {},
+      (err, token) => {
+        if (err) throw err;
+        res.cookie('token', token).json({
+          id:userDoc._id,
+          username,
+        });
+      }
+    )
   }else{
     res.status(400).json("Wrong Credentials!")
   }
+})
+
+app.get('/profile', (req,res)=>{
+  const {token} = req.cookies;
+
+  if (!token) {
+    return res.status(401).json("No token");
+  }
+
+  jwt.verify(token, secret, {}, (err, info)=>{
+    if(err) throw err; 
+    res.json(info) // info from jwt.verify() contains the data stored inside that token
+  })
+})
+
+app.post('/logout', async (req, res)=>{
+  res.cookie('token', '').json('ok')
 })
 
 app.listen(4000, () => {
