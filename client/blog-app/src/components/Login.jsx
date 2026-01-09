d7ade527ceeb3ffe4e700264f3843c09adcaa3b7
CoderFish1 <shreypandey22005@gmail.com>
2026-01-09 06:51:34 +0530

    login page

--- client/blog-app/src/components/Login.jsx
+++ client/blog-app/src/components/Login.jsx
@@ -1,32 +1,48 @@
 import React, { useState } from "react";
+import { Navigate } from "react-router-dom";
 
 const Login = () => {
    const[username, setUsername] = useState('')
    const[password, setPassword] = useState('')
+   const[redirect, setredirect] = useState(false)
+   const[error, setError] = useState(false)
 
 async function login(e){
   e.preventDefault();
 
-  await fetch("http://localhost:4000/login",{
+  const response = await fetch("http://localhost:4000/login",{
     method: 'POST',
     body: JSON.stringify({username, password}),
     headers: {'Content-Type':'application/json'},
+    credentials: 'include' // to save cookies in react app
   })
+  if(response.ok){
+    response.json().then(userinfo =>{
+      setredirect(true);
+    })
+    
+  }else{
+    setError(true)
+  }
 }
 
+if(redirect){
+  return <Navigate to={'/'}/>
+}
   return (
     <div>
+      {error && <p>Wrong credentials</p>}
       <form className="flex flex-col mt-5 " onSubmit={login}>
         <h1 className="mx-auto text-4xl font-bold">Login</h1>
         <input
           type="text" value={username} onChange={e=>setUsername(e.target.value)}
           className="mx-auto w-xl p-2 m-4 border-2 rounded-4xl hover:bg-gray-300 "
-          placeholder="username"
+          placeholder="Username"
         />
         <input
           type="password" value={password} onChange={e=>setPassword(e.target.value)}
           className="mx-auto w-xl p-2 m-4 border-2 rounded-4xl hover:bg-gray-300"
-          placeholder="password"
+          placeholder="Password"
         />
         <button className="border w-[8vw] p-2 mt-2 font-medium rounded-3xl bg-gray-400 mx-auto hover:bg-gray-500">
           Login
--- client/blog-app/src/components/Navbar.jsx
+++ client/blog-app/src/components/Navbar.jsx
@@ -1,18 +1,52 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useEffect,useState } from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
+  const [username, setUsername] = useState(null);
+  useEffect(() => {
+    fetch("http://localhost:4000/profile", {
+      credentials: "include",
+    }).then((response) => {
+        response.json().then((userInfo) => {
+          setUsername(userInfo.username)
+        });
+      });
+  }, []);
+
+  function logOut(){
+    fetch("http://localhost:4000/logout",{
+      credentials: 'include',
+      method: 'POST',
+    });
+    setUsername(null);
+  }
   return (
     <div>
       <div className=" nav flex justify-between p-4">
-          <Link to={"/"} className="logo font-bold text-2xl">BlogLOGO</Link>
-          <ul className="flex gap-4">
-            <Link className='font-medium' to={"/login"}>Login</Link>
-            <Link className='font-medium' to={"/register"}>Register</Link>
-          </ul>
-        </div>
+        <Link to={"/"} className="logo font-bold text-2xl">
+          BlogLOGO
+        </Link>
+        <ul className="flex gap-4">
+          {username && (
+            <>
+              <Link to="/create">Create new post</Link>
+              <a onClick={logOut}>Logout</a>
+            </>
+          )}
+          {!username && (
+            <>
+              <Link className="font-medium" to={"/login"}>
+                Login
+              </Link>
+              <Link className="font-medium" to={"/register"}>
+                Register
+              </Link>
+            </>
+          )}
+        </ul>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
